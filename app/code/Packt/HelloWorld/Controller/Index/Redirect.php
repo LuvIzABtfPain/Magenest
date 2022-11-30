3000d789c18a525d6e125f9d70935d631c453a66
Manh Hung <hung194292@sis.hust.edu.vn>
2022-11-30 20:32:43 +0700

    Added my project

---
+++ app/code/Packt/HelloWorld/Controller/Index/Redirect.php
@@ -0,0 +1,11 @@
+<?php
+namespace Packt\HelloWorld\Controller\Index;
+use Magento\Framework\App\Action\Action;
+
+class Redirect extends Action
+{
+    public function execute()
+    {
+        $this->_redirect('helloworld');
+    }
+}
